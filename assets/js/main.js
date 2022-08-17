e588e87c3a9ebc96d072860a5e810aec443c0138
Ale190107 <alejandrolopez190107@gmail.com>
2022-08-17 17:13:45 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function changeImg(){
 
     if($changeFocus.src.match("assets/img/Focus-On.png") && $changeSwitch.src.match("assets/img/on-or.png")) {
         $changeFocus.src = "assets/img/Focus-Off.png";
-        $cchangeSwitch.src = "assets/img/off-or.png";
+        $changeSwitch.src = "assets/img/off-or.png";
     }else{
         $changeFocus.src = "assets/img/Focus-On.png";
         $changeSwitch.src = "assets/img/on-or.png";
